a93320d0502177c62dfeab45b27979316cafe50e
Kriyu <thcavailles@laposte.net>
2018-11-14 16:39:36 +0100

    Complete prePersist method (UserListener) to encode password of user and set the role (member)

--- src/AppBundle/EventListener/UserListener.php
+++ src/AppBundle/EventListener/UserListener.php
@@ -2,12 +2,30 @@
 
 namespace AppBundle\EventListener;
 
+use AppBundle\Entity\User;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserListener
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function prePersist(LifecycleEventArgs $args)
     {
-        
+        $user = $args->getObject();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $encoded = $this->encoder->encodePassword($user, $user->getPassword());
+
+        $user->setPassword($encoded);
+        $user->setRoles(['ROLE_MEMBER']);
     }
 }
