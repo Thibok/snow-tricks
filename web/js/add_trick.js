12e86e2950acd3462f5c6f4be6513bd3c39ed4ef
Kriyu <thcavailles@laposte.net>
2018-12-19 16:31:16 +0100

    Customize medias part to responsive design, add controls icons edit and delete, create preview images default (normal and thumb)

--- web/js/add_trick.js
+++ web/js/add_trick.js
@@ -10,36 +10,78 @@ $(function () {
     function addTrickImage(containerImg) {
         var prototype = containerImg.attr('data-prototype').replace(/__name__/g, length);
         var fileField = $(prototype);
-        var numberOfField = fileField.attr('id').split('_');
+        var idSplit = fileField.attr('id').split('_');
 
         fileField.hide();
         containerImg.append(fileField);
+        createTrickImagePreview();
         length++;
-        $('#addMedia').addClass('mt-3');
-        createTrickImagePreview(numberOfField[3]);
         fileField.trigger('click');
-    }
-
-    function createTrickImagePreview (numberOfField) {
-        var previewImg = $('<img class="card-img-top px-2" src="../img/preview_trick_thumb.png" alt="preview_mini"/>');
-        previewImg.css('width', '200px').css('height', '100px');
-        $('#medias').slick('slickAdd', previewImg);
     }
 
     $('#medias').slick({
         infinite: false,
-        arrows: true,
         slidesToShow: 5,
         slidesToScroll: 5,
+        variableWidth: true,
         responsive: [
             {
-              breakpoint: 1300,
+              breakpoint: 1900,
               settings: {
-                slidesToShow: 4
+                slidesToShow: 4,
+                slidesToScroll: 4,
               }
             },
-        ]
+            {
+              breakpoint: 1555,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            },
+            {
+                breakpoint: 1210,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+            },
+            {
+              breakpoint: 510,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
+
+    function createTrickImagePreview () {
+        var previewImg = $('<img class="border-black" id="trickImg-sm-' + length + '"src="../img/preview_trick_thumb.jpg" alt="preview_mini"/>');
+        previewImg.css('width', '170px').css('height', '100px');
+        previewImgContainer = addControlImg(previewImg);
+
+        $('#medias').slick('slickAdd', previewImgContainer);
+    }
+
+    function addControlImg (trickImagePreview) {
+        var previewImgContainer = $('<div class="d-flex flex-column"></div>');
+        var controlsContainer = $('<div class="d-flex flex-row justify-content-around py-1 mt-2 border-black"></div>')
+        controlsContainer.css('background-color', 'white').css('width', '90px').css('height', '25px').css('margin-left', '80px');
+        var editImg = $('<img class="px-2" src="../img/edit.png"/>');
+        var deleteImg = $('<img class="px-2" src="../img/delete.png"/>');
+
+        controlsContainer.append(editImg);
+        controlsContainer.append(deleteImg);
+
+        previewImgContainer.append(trickImagePreview);
+        previewImgContainer.append(controlsContainer);
+
+        return previewImgContainer;
+    }
 
     $('input[type=file]').click(function () {
 
