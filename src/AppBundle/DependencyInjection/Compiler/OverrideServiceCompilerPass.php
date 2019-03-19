b3644864ee6f69d0603ecd666b4ed99450ba1c29
Kriyu <thcavailles@laposte.net>
2019-03-19 11:27:41 +0100

    Update class diagram, add phpdoc

--- src/AppBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ src/AppBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -1,12 +1,22 @@
 <?php
 
+/**
+ * Set services in container to public
+ */
+
 namespace AppBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * OverrideServiceCompilerPass
+ */
 class OverrideServiceCompilerPass implements CompilerPassInterface
 {
+    /**
+     * @inheritDoc
+     */
     public function process(ContainerBuilder $container)
     {
         $container->getDefinition('assetic.filter_manager')->setPublic(true);
